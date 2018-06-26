0d00f2fd19785c8318b653099b4b97eb651bcd57
marceickhoff <mail@marceickhoff.com>
2018-06-26 14:32:07 +0200

    Removed charset comment

--- theme/header.php
+++ theme/header.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="<?php echo Localization::get(); ?>">
 <head>
-	<meta charset="UTF-8"> <!-- HTML5 -->
+	<meta charset="UTF-8">
 	<title><?php echo Meta::get('website_title'); ?> &bull; <?php echo Meta::get('title'); ?></title>
 	<meta name="robots" content="<?php echo Meta::get('robots'); ?>">
 	<meta name="description" content="<?php echo Meta::get('description'); ?>">

844cabfe86fe83daa9d25d90bc6ddd121816a56a
marceickhoff <mail@marceickhoff.com>
2018-06-26 14:33:41 +0200

    Removed viewport meta from default template

--- theme/header.php
+++ theme/header.php
@@ -5,7 +5,6 @@
 	<title><?php echo Meta::get('website_title'); ?> &bull; <?php echo Meta::get('title'); ?></title>
 	<meta name="robots" content="<?php echo Meta::get('robots'); ?>">
 	<meta name="description" content="<?php echo Meta::get('description'); ?>">
-	<meta name="viewport" content="width=device-width; initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 	<?php Assets::get('head'); ?>
 </head>
 <body>

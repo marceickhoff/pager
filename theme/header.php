ad48fe2cfe33c8b578f929097e33ccc501ad1856
marceickhoff <mail@marceickhoff.com>
2018-09-07 16:21:15 +0200

    Switched title parts in default header

--- theme/header.php
+++ theme/header.php
@@ -2,7 +2,7 @@
 <html lang="<?php echo Localization::get(); ?>">
 <head>
 	<meta charset="UTF-8">
-	<title><?php echo Meta::get('website_title'); ?> &bull; <?php echo Meta::get('title'); ?></title>
+	<title><?php echo Meta::get('title'); ?> &bull; <?php echo Meta::get('website_title'); ?></title>
 	<meta name="robots" content="<?php echo Meta::get('robots'); ?>">
 	<meta name="description" content="<?php echo Meta::get('description'); ?>">
 	<?php Assets::get('head'); ?>

bf242f4f759bb592adbed8c4bb03c11c84b484e6
marceickhoff <mail@marceickhoff.com>
2018-09-19 13:20:24 +0200

    Fixed base path in subdirectories

--- system/config.php
+++ system/config.php
@@ -1,7 +1,7 @@
 <?php
 	Config::set(
 		[
-			'base_path' => trim(str_replace('\\', '/', pathinfo($_SERVER['PHP_SELF'], PATHINFO_DIRNAME)), '/'), //Replace backslashes for Windows support
+			'base_path' => rtrim(str_replace('\\', '/', pathinfo($_SERVER['PHP_SELF'], PATHINFO_DIRNAME)), '/'), //Replace backslashes for Windows support
 			'default_localization' => 'en',
 			'default_localization_redirect' => false
 		]

effe3405675be9d78a40eab676fd5a125ee5533d
marceickhoff <mail@marceickhoff.com>
2019-04-20 21:11:03 +0200

    Added redirect and refresh methods to Router

--- system/classes/Router.class.php
+++ system/classes/Router.class.php
@@ -15,6 +15,24 @@
 				//Custom routes
 			];
 		}
+
+		/**
+		 * Redirects the client.
+		 * @param string $target Relative target URL
+		 * @param int $http_response_code (optional, default: 302) HTTP response code
+		 */
+		public static function redirect($target, $http_response_code = 302) {
+			header('Location: '.BASE_PATH.'/'.$target, true, $http_response_code);
+			die();
+		}
+
+		/**
+		 * Refreshes the current page.
+		 */
+		public static function refresh() {
+			header("Refresh:0");
+			die();
+		}
 	}
 
 ?>
